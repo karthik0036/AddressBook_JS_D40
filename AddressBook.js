9b12e376fcabeb4b451458b4ec6fe53092e2989b
Karthik <jeefkarthik036@gmail.com>
2022-05-12 19:13:13 +0530

    Validation of User Contact Details

--- AddressBook.js
+++ AddressBook.js
@@ -2,29 +2,19 @@
 const ContactDetails = require("./ContactDetails.js");
 const prompt = require('prompt-sync')();
 
-// ability to add details to ContactDetails class
-var contactInfo = new ContactDetails("Jon","Snow","North","Mysre","Karnataka","511232","9123456789"
-                  ,"jon@gmail.com");
-
-console.log(contactInfo.toString());
-
-contactInfo.firstName = "Ned";
-contactInfo.lastName = "Stark";
-contactInfo.address = "South";
-contactInfo.city = "Mandya";
-contactInfo.state = "Karnataka";
-contactInfo.zip = "534343";
-contactInfo.phoneNumber = "1234556789";
-contactInfo.email = "nedstark@gmail.com";
-console.log(contactInfo.toString());
 
 //Ability to add user-defined values to the PersonInfoData
-contactInfo.firstName = prompt("Enter First Name : "); 
-contactInfo.lastName = prompt("Enter Last Name : "); 
-contactInfo.address = prompt("Enter Address : "); 
-contactInfo.city = prompt("Enter City : "); 
-contactInfo.state = prompt("Enter State : "); 
-contactInfo.zip = prompt("Enter Zip : "); 
-contactInfo.phoneNumber = prompt("Enter Phone Number : "); 
-contactInfo.email = prompt("Enter Email : "); 
-console.log(contactInfo.toString());
+try {
+    let contactInfo = new ContactDetails();
+    contactInfo.fName = prompt("Enter First Name : ");
+    contactInfo.lName = prompt("Enter Last Name : ");
+    contactInfo.addressName = prompt("Enter Address : ");
+    contactInfo.cityName = prompt("Enter City : ");
+    contactInfo.stateName = prompt("Enter State : ");
+    contactInfo.zipNumber = prompt("Enter Zip : ");
+    contactInfo.phoneNo = prompt("Enter Phone Number : ");
+    contactInfo.emailID = prompt("Enter Email : ");
+    console.log(contactInfo.toString());
+} catch (e) {
+    console.error(e);
+}
